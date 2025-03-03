fea7fce380a23682ebdc8852fd9c2eb55ec8eb80
mostafa9922 <mostafa9922m@gmai.com>
2025-03-03 15:54:08 +0200

    V1.2

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,28 @@ const App = () => {
   const [products, setProducts] = useState([
     { id: 1, name: "chipsy", price: 100, count: 0 },
     { id: 2, name: "pepsi", price: 200, count: 0 },
-    { id: 3, name: "cigartes", price: 300, count: 0 },
+    { id: 3, name: "cigarettes", price: 300, count: 0 },
     { id: 4, name: "baneh", price: 400, count: 0 },
   ]);
 
   const [theme, setTheme] = useState("light");
 
   const increment = (id) => {
-    const newProducts = products.map((product) => {
-      if (product.id === id) {
-        product.count += 1;
-      }
-      return product;
-    });
-    setProducts(newProducts);
+    setProducts(
+      products.map((product) =>
+        product.id === id ? { ...product, count: product.count + 1 } : product
+      )
+    );
   };
 
   const decrement = (id) => {
-    const newProducts = products.map((product) => {
-      if (product.id === id && product.count > 0) {
-        product.count -= 1;
-      }
-      return product;
-    });
-    setProducts(newProducts);
+    setProducts(
+      products.map((product) =>
+        product.id === id && product.count > 0
+          ? { ...product, count: product.count - 1 }
+          : product
+      )
+    );
   };
 
   const changeTheme = () => {
@@ -39,25 +37,29 @@ const App = () => {
   };
 
   const reset = () => {
-    const newProducts = products.map((product) => {
-      product.count = 0;
-      return product;
-    });
-    setProducts(newProducts);
+    setProducts(products.map((product) => ({ ...product, count: 0 })));
   };
+
   return (
     <div data-theme={theme}>
-      <Header />
-      <Cart
-        products={products}
-        increment={increment}
-        decrement={decrement}
-        reset={reset}
-        changeTheme={changeTheme}
-        theme={theme}
-      />
       <Routes>
-        <Route path='/App' element={<App />}></Route>
+        {/* Show Header only on the home page */}
+        <Route
+          path='/'
+          element={
+            <>
+              <Header />
+              <Cart
+                products={products}
+                increment={increment}
+                decrement={decrement}
+                reset={reset}
+                changeTheme={changeTheme}
+                theme={theme}
+              />
+            </>
+          }
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <div>
+    <div className="h-screen">
       <section className='flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800'>
         <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
           <div className='max-w-md text-center'>
